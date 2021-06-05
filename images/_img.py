83249f9eac07b5a650b6b715f7742cb149597436
Anand Chitipothu <anandology@gmail.com>
2021-06-05 12:41:05 +0530

    Reduced the size of the generated image.
    It was looking too big in the readme.

--- images/_img.py
+++ images/_img.py
@@ -7,5 +7,11 @@ def render(*shapes):
         rect(x=-150, y=-150, width=300, height=300, fill="white", stroke="#ddd"),
         line(x1=-150, y1=0, x2=150, y2=0, stroke="#ddd"),
         line(x1=0, y1=-150, x2=0, y2=150, stroke="#ddd"))
-    shape = combine(bg, *shapes)
-    print(shape.as_svg())
+
+    shape = group(
+        [bg, *shapes],
+        stroke_width=2 # increase the stroke-width to compensate for scaling
+    )
+    shape = scale(shape, xs=0.5)
+    svg = SVG([shape], width=150, height=150)
+    print(svg)
